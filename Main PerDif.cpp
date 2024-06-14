38601194191c493b8820689c2e4ddd7b39772a3d
Khlfnalvr <khalfani.alvaro@aol.com>
2024-06-14 20:36:45 +0700

    ver 1.3

--- Main PerDif.cpp
+++ Main PerDif.cpp
@@ -31,22 +31,22 @@ void menuPerDif() {
     switch (pilihan) {
         case 1: //Euler
             cout << "Penyelesaian Menggunakan Metode Euler" << endl;
-            cout << "x"<< setw(16)<< "y"<<setw(16)<<"y'"<<setw(16)<<"hy'"<<setw(16)<<"y+hy'\n";
+            cout << setw(5) << "x"<< setw(16)<< "y"<<setw(16)<<"y'"<<setw(16)<<"hy'"<<setw(16)<<"y+hy'\n";
             cout << "__________________________________________________________________________\n";
             yi = yo;
             while(x0<x){
                 y=yi+h*f(x0,yi);
-                cout << x0 << setw(16) << yi << setw(16) << f(x0,yi) << setw(16) << h*f(x0,yi) << setw(16) << y << "\n";
+                cout<< setw(5) << x0 << setw(16) << yi << setw(16) << f(x0,yi) << setw(16) << h*f(x0,yi) << setw(16) << y << "\n";
                 yi=y;
                 x0=x0+h;
             }
-            cout << x0 << "\t" << yi << "\n";
+            cout << setw(5) << x0 << setw(16)  << yi << "\n";
             cout << "__________________________________________________________________________\n";
             cout << "Nilai y pada x = " << x << " adalah " << y << "\n\n";
             break;
         case 2: //Euler Termodifikasi
             cout << "Penyelesaian Menggunakan Metode Euler Termodifikasi" << endl;
-            cout<<"x"<<setw(16)<<"y"<<setw(16)<<"hy'"<<setw(16)<<"y_new"<<setw(16)<<"hy_new'"<<setw(16)<<"hy'avg"<<setw(16)<<"y_n+1"<<endl;
+            cout<< setw(5)<<"x"<<setw(16)<<"y"<<setw(16)<<"hy'"<<setw(16)<<"y_new"<<setw(16)<<"hy_new'"<<setw(16)<<"hy'avg"<<setw(16)<<"y_n+1"<<endl;
             cout<<"--------------------------------------------------------------------------------------------------\n"; 
             while(fabs(x-x0)>0.0000001){   
                 dy1=h*f(x0,yo);       
@@ -54,27 +54,28 @@ void menuPerDif() {
                 dy2=h*f(x0+h,y_i);     
                 dy_avg=(dy1+dy2)/2.0;  
                 y_n=yo+dy_avg;             
-                cout<<x0<<setw(16)<<yo<<setw(16)<<dy1<<setw(16)<<y_i<<setw(16)<<dy2<<setw(16)<<dy_avg<<setw(16)<<y_n<<endl;
+                cout<< setw(5)<<x0<<setw(16)<<yo<<setw(16)<<dy1<<setw(16)<<y_i<<setw(16)<<dy2<<setw(16)<<dy_avg<<setw(16)<<y_n<<endl;
                 x0=x0+h;           
                 yo=y_n;           
             }
-            cout<<x0<<setw(16)<<yo<<endl;
+            cout<< setw(5)<<x0<<setw(16)<<yo<<endl;
             cout << "____________________________________________________________________________________________________________\n";
             cout << "Nilai y pada x = " << x << " adalah " << yo << "\n\n";
             break;
         case 3: //Runge Kutta
             cout << "Penyelesaian Menggunakan Runge Kutta" << endl;
+            cout << setw(5) << "x" << setw(16) << "y" << setw(16) << "k1" << setw(16) << "k2" << setw(16) << "k3" << setw(16) << "k4" << setw(16) << "K average" << "\n";
             while(x0<x){
                 k1=h*f(x0,yo);
                 k2=h*f(x0+h/2.0,yo+k1/2.0);
                 k3=h*f(x0+h/2.0,yo+k2/2.0);
                 k4=h*f(x0+h,yo+k3);
-                cout << x0 << setw(16) << yo << setw(16) << k1 << setw(16) << k2 << setw(16) << k3 << setw(16) << k4 << setw(16) << 1/6.0*(k1+2*k2+2*k3+k4) << "\n";
+                cout << setw(5) << x0 << setw(16) << yo << setw(16) << k1 << setw(16) << k2 << setw(16) << k3 << setw(16) << k4 << setw(16) << 1/6.0*(k1+2*k2+2*k3+k4) << "\n";
                 y=yo+1/6.0*(k1+2*k2+2*k3+k4);
                 yo=y;
                 x0=x0+h;
              }
-            cout << x0 << "\t" << yo << "\n";
+            cout << setw(5) << x0 << setw(16)  << yo << "\n";
             cout << "____________________________________________________________________________________________________________\n";
             cout << "Nilai y pada x = " << x << " adalah " << y << "\n\n";
             
